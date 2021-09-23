b21a25ac0a7504489d8941886cc5a21fc9f38f3b
Giorgio Bolzon <gbolzon@inogs.it>
2021-09-23 10:48:31 +0200

    TimeList_generator.py takes delta (days,hours...) as arguments, no more as a string

--- TimeList_generator.py
+++ TimeList_generator.py
@@ -11,19 +11,39 @@ def argument():
                                 type = str,
                                 required = True,
                                 help = '20120110-12:00:00')
-    parser.add_argument(   '--delta',"-d",
+    parser.add_argument(   '--days',
+                                type = int,
+                                required = False,
+                                help = '')
+    parser.add_argument(   '--hours',
+                                type = int,
+                                required = False,
+                                help = '')
+    parser.add_argument(   '--min',
+                                type = int,
+                                required = False,
+                                help = '')
+    parser.add_argument(   '--months',
+                                type = int,
+                                required = False,
+                                help = '')
+    parser.add_argument(   '--dateformat',
                                 type = str,
-                                required = True,
-                                help = 'days = 1')    
+                                required = False,
+                                default = '%Y%m%d-%H:%M:%S')
     
     
     return parser.parse_args()
 
 args = argument()
 from commons import genUserDateList as DL
+if args.days  : TL   = DL.getTimeList(args.datestart, args.dateend, days  = args.days   )
+if args.hours : TL   = DL.getTimeList(args.datestart, args.dateend, hours = args.hours  )
+if args.min   : TL   = DL.getTimeList(args.datestart, args.dateend, minutes  = args.min )
+if args.months: TL   = DL.getTimeList(args.datestart, args.dateend, months = args.months)
 
-TL   = DL.getTimeList(args.datestart, args.dateend, args.delta )
-dateFormat="%Y%m%d-%H:%M:%S"
+
+#dateFormat=""
 for t in TL:
-    print(t.strftime(dateFormat))
+    print(t.strftime(args.dateformat))
 
