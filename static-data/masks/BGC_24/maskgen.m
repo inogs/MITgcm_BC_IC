b8f51f11f8a73a3614830256e6a94e2581ff9134
Valeria Di Biagio <vdibiagi@login03.g100.cineca.it>
2022-03-15 19:15:30 +0100

    maskgen for BGC at 1/24

---
+++ static-data/masks/BGC_24/maskgen.m
@@ -0,0 +1,44 @@
+tmask = ncread('meshmask.nc','tmask');
+depth = ncread('meshmask.nc','nav_lev');
+lon = ncread('meshmask.nc','nav_lon');
+lat = ncread('meshmask.nc','nav_lat');
+umask = ncread('meshmask.nc','umask');
+vmask = ncread('meshmask.nc','vmask');
+
+
+
+%
+nlon = size(tmask,1);
+nlat = size(tmask,2);
+nlev = size(tmask,3);
+%
+
+ncid = netcdf.create('mask.nc','CLOBBER');
+
+%
+nx = netcdf.defDim(ncid,'lon', nlon);
+ny = netcdf.defDim(ncid,'lat',nlat);
+nz = netcdf.defDim(ncid,'depth',nlev);
+%
+varid1 = netcdf.defVar(ncid,'lon','NC_FLOAT',nx);
+netcdf.putAtt(ncid,varid1,'type','FLOAT'); 
+varid2 = netcdf.defVar(ncid,'lat','NC_FLOAT',ny);
+netcdf.putAtt(ncid,varid2,'type','FLOAT');       
+varid3 = netcdf.defVar(ncid,'depth','NC_FLOAT',nz);
+varid4 = netcdf.defVar(ncid,'tmask','NC_BYTE',[nx, ny, nz]);
+varid5 = netcdf.defVar(ncid,'umask','NC_BYTE',[nx, ny, nz]);
+varid6 = netcdf.defVar(ncid,'vmask','NC_BYTE',[nx, ny, nz]);
+
+
+%
+netcdf.endDef(ncid);
+
+%
+netcdf.putVar(ncid,varid1,lon(:,1));
+netcdf.putVar(ncid,varid2,lat(1,:));
+netcdf.putVar(ncid,varid3,depth(:,1));
+netcdf.putVar(ncid,varid4,tmask);
+netcdf.putVar(ncid,varid5,umask);
+netcdf.putVar(ncid,varid6,vmask);
+%
+netcdf.close(ncid);
