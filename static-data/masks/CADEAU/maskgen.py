15365da069f09b5e18dd19c56316fb80faee5326
Valeria Di Biagio <vdibiagio@ogs.it>
2024-07-10 16:53:42 +0200

    Adding rivermask variable in the output mask file

--- static-data/masks/CADEAU/maskgen.py
+++ static-data/masks/CADEAU/maskgen.py
@@ -69,7 +69,49 @@ for ji in range(jpi):
         for jk in range(LEVELS[jj,ji]):
             tmask[jk, jj,ji] = True
 
-
+rivermask = np.zeros((jpk,jpj,jpi), bool)
+shift=128
+#  1 - Krka 
+rivermask[0:2,160-shift:166-shift,464] = True
+#  2 - Zrmanja 
+rivermask[0:2,230-shift,414:420] = True
+#  3 - HPP Senj + Crikvenica 
+rivermask[0:2,344-shift,319:325] = True
+#  4 - Bakarac + Rjecina 
+rivermask[0:2,365-shift:371-shift,286] = True
+#  5 - Rasa 
+rivermask[0:2,319-shift:325-shift,236] = True
+#  6 - Mirna 
+rivermask[0:2,364-shift,176:182] = True
+#  7 - Dragonja 
+rivermask[0:2,385-shift,176:182] = True
+#  8 - Rizana 
+rivermask[0:2,395-shift,194:200] = True
+#  9 - Timavo
+rivermask[0:2,423-shift,178:184] = True
+# 10 - Isonzo
+rivermask[0:2,417-shift,165:171] = True
+rivermask[0:2,417-shift:422-shift,165] = True
+# 11 - Tagliamento
+rivermask[0:2,405-shift:411-shift,112] = True
+# 12 - Livenza
+rivermask[0:2,399-shift:405-shift,92-10] = True
+# 13 - Piave
+rivermask[0:2,391-shift:397-shift,59+5] = True
+# 14 - Sile
+rivermask[0:2,385-shift:391-shift,47-2] = True
+# 15 - Brenta
+rivermask[0:2,347-shift,7:13] = True
+# 16 - Adige
+rivermask[0:2,343-shift,9:15] = True
+# 17 - Po
+rivermask[0:2,317-shift:319-shift,3:42] = True # 2 x-cells deleted from mouth for each y-row
+rivermask[2,317-shift:319-shift,3:43] = True   # 1 x-cell deleted from mouth for each y-row
+rivermask[0:3,297-shift:319-shift,24] = True   # 1 y-cell deleted from mouth 
+# 18 - Reno + Lamone
+rivermask[0:2,270-shift,2:8] = True
+# 19 - Foglia
+rivermask[0:2,185-shift,81:87] = True
 
 ncOUT = NC.netcdf_file(maskfile,"w")
 ncOUT.createDimension("lon",jpi)
@@ -101,7 +143,6 @@ lon2d=  np.repeat(np.array(Lon,ndmin=2),jpj,axis=0)
 lat2d = np.repeat(np.array(Lat,ndmin=2).T,jpi,axis=1)
 ncvar[:] = lon2d
 
-
 ncvar    = ncOUT.createVariable("nav_lat", 'f', ("lat","lon"))
 ncvar[:] = lat2d
 
@@ -109,6 +150,9 @@ ncvar = ncOUT.createVariable("e1t",'f',('z_a','z_a','lat','lon'))
 ncvar[:]=0
 ncvar = ncOUT.createVariable("e2t",'f',('z_a','z_a','lat','lon'))
 ncvar[:]=0
+
+ncvar    = ncOUT.createVariable("rivermask", 'b', ("depth","lat","lon"))
+ncvar[:] = rivermask
 
 ncOUT.close()
 
