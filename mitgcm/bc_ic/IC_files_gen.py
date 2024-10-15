94770f0a766da5ffe0484e8f5183ed671077c7f0
Jacopo Nespolo <jacopo.nespolo@exact-lab.it>
2024-10-15 16:59:21 +0200

    formatting and linting

--- mitgcm/bc_ic/IC_files_gen.py
+++ mitgcm/bc_ic/IC_files_gen.py
@@ -1,61 +1,69 @@
-from .general import mask, space_intepolator_griddata, NetCDF_phys_Files, NetCDF_phys_Vars
-from bitsea.commons.dataextractor import DataExtractor
-import numpy as np
-import os
-from bitsea.commons.mask import Mask
-from bitsea.commons import netcdf4
-import scipy.io as NC
-from bitsea.commons.utils import addsep, file2stringlist
 import argparse
+import os
+
+import numpy as np
+import scipy.io as NC
+from bitsea.commons import netcdf4
+from bitsea.commons.dataextractor import DataExtractor
+from bitsea.commons.mask import Mask
+from bitsea.commons.utils import addsep
+from bitsea.commons.utils import file2stringlist
+
+from .general import mask
+from .general import NetCDF_phys_Files
+from .general import NetCDF_phys_Vars
+from .general import space_intepolator_griddata
 
 
 def argument():
-    parser = argparse.ArgumentParser(description='''Creates ready IC files for MITgcm 
-                               both physical and biogeochemical. Since input data come from other external run,
-                               a linear interpolation is performed. 
-                               If argument MODELVARLIST is given with biogeochemical variable list, IC for these BIO variables
-                               will be created. Else, physical IC will be created.
-                               ''')
-    parser.add_argument('--inputdir', '-i',
-                        type=str,
-                        required=True,
-                        help='/some/path/'
-                        )
+    parser = argparse.ArgumentParser(
+        description=(
+            "Creates ready IC files for MITgcm both physical and "
+            "biogeochemical. Since input data come from other external run,"
+            "a linear interpolation is performed. If argument MODELVARLIST "
+            "is given with biogeochemical variable list, IC for these BIO "
+            "variables will be created. Else, physical IC will be created."
+        )
+    )
+    parser.add_argument(
+        "--inputdir", "-i", type=str, required=True, help="/some/path/"
+    )
 
-    parser.add_argument('--outmaskfile', '-m',
-                        type=str,
-                        required=True,
-                        help='/some/path/outmask.nc')
-    parser.add_argument('--outputdir', "-o",
-                        type=str,
-                        required=True,
-                        help='/some/path/')
-    parser.add_argument('--nativemask',
-                        type=str,
-                        required=True,
-                        help='''NetCDF File name of the mask on which inputs are defined.
-                                ''')
-    parser.add_argument('--modelvarlist', "-v",
-                        type=str,
-                        default=None,
-                        help='''/some/path/filename,
-                                Do not define it to get physical IC.''')
-    parser.add_argument('--time', "-t",
-                        type=str,
-                        required=True,
-                        help='''/some/path/filename.
-                                A file with an only time, the restart time
-                                       ''')
+    parser.add_argument(
+        "--outmaskfile",
+        "-m",
+        type=str,
+        required=True,
+        help="/some/path/outmask.nc",
+    )
+    parser.add_argument(
+        "--outputdir", "-o", type=str, required=True, help="/some/path/"
+    )
+    parser.add_argument(
+        "--nativemask",
+        type=str,
+        required=True,
+        help="NetCDF File name of the mask on which inputs are defined.",
+    )
+    parser.add_argument(
+        "--modelvarlist",
+        "-v",
+        type=str,
+        default=None,
+        help="/some/path/filename, Do not define it to get physical IC.",
+    )
+    parser.add_argument(
+        "--time",
+        "-t",
+        type=str,
+        required=True,
+        help="/some/path/filename. A file with an only time, the restart time",
+    )
 
     return parser.parse_args()
 
 
-def writeCheckFile(*,
-                   M,
-                   outputdir,
-                   var,
-                   Mask1,
-                   Mask2):
+def writeCheckFile(*, M, outputdir, var, Mask1, Mask2):
     Mcheck = M.copy()
 
     checkfile = outputdir + "CHECK/" + "IC_" + var + ".nc"
@@ -63,20 +71,15 @@ def writeCheckFile(*,
     NCout.createDimension("Lon", Mask2.Lon.size)
     NCout.createDimension("Lat", Mask2.Lat.size)
     NCout.createDimension("Depth", Mask2.jpk)
-    ncvar = NCout.createVariable(var, 'f', ('Depth', 'Lat', 'Lon'))
-    setattr(ncvar, 'missing_value', 1.e+20)
+    ncvar = NCout.createVariable(var, "f", ("Depth", "Lat", "Lon"))
+    setattr(ncvar, "missing_value", 1.0e20)
     ncvar[:] = Mcheck
     NCout.close()
 
 
-def gen_ic_files(*,
-                 inputdir,
-                 outputdir,
-                 nativemask,
-                 time,
-                 outmaskfile,
-                 modelvarlist):
-
+def gen_ic_files(
+    *, inputdir, outputdir, nativemask, time, outmaskfile, modelvarlist
+):
     rank = 0
     nranks = 1
 
@@ -94,19 +97,16 @@ def gen_ic_files(*,
 
         for var in MODELVARS[rank::nranks]:
             inputfile = INPUTDIR + "ave." + TIMELIST[0] + "." + var + ".nc"
-            outBinaryFile = OUTPUTDIR + 'IC_' + var + ".dat"
+            outBinaryFile = OUTPUTDIR + "IC_" + var + ".dat"
             print("rank %d working on %s" % (rank, outBinaryFile), flush=True)
             B = DataExtractor(Mask_bitsea1, inputfile, var).values
 
             M = space_intepolator_griddata(Mask2, Mask1, B)
 
             writeCheckFile(
-                M=M,
-                outputdir=OUTPUTDIR,
-                var=var,
-                Mask1=Mask1,
-                Mask2=Mask2)
-            F = open(outBinaryFile, 'wb')
+                M=M, outputdir=OUTPUTDIR, var=var, Mask1=Mask1, Mask2=Mask2
+            )
+            F = open(outBinaryFile, "wb")
             for jk in range(Mask2.jpk):
                 F.write(M[jk, :, :])
             F.close()
@@ -114,18 +114,24 @@ def gen_ic_files(*,
     else:
         MODELVARS = ["U", "V", "T", "S"]
         for var in MODELVARS[rank::nranks]:
-            inputfile = INPUTDIR + TIMELIST[0][:8] + \
-                "_" + NetCDF_phys_Files[var] + ".nc"
+            inputfile = (
+                INPUTDIR
+                + TIMELIST[0][:8]
+                + "_"
+                + NetCDF_phys_Files[var]
+                + ".nc"
+            )
             outBinaryFile = OUTPUTDIR + "IC_" + var + ".dat"
             print("rank %d working on %s" % (rank, outBinaryFile), flush=True)
 
             B = netcdf4.readfile(inputfile, NetCDF_phys_Vars[var])[
-                0, :Mask1.jpk, :, :]
+                0, : Mask1.jpk, :, :
+            ]
             B[~Mask1.tmask] = np.NaN
 
             M = space_intepolator_griddata(Mask2, Mask1, B)
             writeCheckFile()
-            F = open(outBinaryFile, 'wb')
+            F = open(outBinaryFile, "wb")
             for jk in range(Mask2.jpk):
                 F.write(M[jk, :, :])
             F.close()
@@ -139,4 +145,5 @@ if __name__ == "__main__":
         nativemask=args.nativemask,
         time=args.time,
         outmaskfile=args.outmaskfile,
-        modelvarlist=file2stringlist(args.modelvarlist))
+        modelvarlist=file2stringlist(args.modelvarlist),
+    )
