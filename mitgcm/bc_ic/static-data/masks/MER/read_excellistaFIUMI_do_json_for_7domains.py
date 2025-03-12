adfdfc08d9d8345003daef8b8a9cae1d6e0aca49
Fabio Giordano <fgiordano@ogs.it>
2025-03-12 11:48:03 +0100

    Scripts to: 1) read point sources or rivers positions, CMS salinity and  and write them to a json; 2) read the json and create the masks and release arrays needed by the MITgcm for the pollutant tracer advection. Plus corresponding starting data (.xlsx)

---
+++ mitgcm/bc_ic/static-data/masks/MER/read_excellistaFIUMI_do_json_for_7domains.py
@@ -0,0 +1,46 @@
+# read the listaFIUMI_MER excel file and select the domain from 1 to 7 and 
+# build the json files with the coordinate of the riversource for E.Coli tracers
+import pandas as pd
+import json
+import os
+
+# Allegato 1 of sewage discharge locations 
+excel_file = 'listaFIUMI_MER.xlsx'
+# select the n. of domain from 1 to 7 and name the json file
+
+domains=['NAD','SAD','ION','SIC','TYR','LIG','SAR']
+
+
+# Extract the Excel file name without extension
+file_name = os.path.splitext(os.path.basename(excel_file))[0]
+
+# Read the Excel file into a DataFrame
+df = pd.read_excel(excel_file)
+
+for id, namedomain in enumerate(domains):
+
+  # Filter rows where the value in column 'Dominio' is id+1 (valid number from 1 to 7)
+  filtered_df = df[df['Dominio'] == id+1]
+
+  # Select only the interesting columns 
+  selected_columns = filtered_df[['rivername', 'lat_mouth', 'lon_mouth','MEAN_2011_2023', 'catchment','Region']]
+
+  # Convert the selected rows to a list of dictionaries
+  filtered_rows = selected_columns.to_dict(orient='records')
+
+  # Create the final JSON structure
+  output_data = {
+      "file_name_origin": file_name,
+      "domain_number": id+1,
+      "domain_name": domains[id],
+      "n_points": len(filtered_rows),
+      "river_points": filtered_rows
+  }
+  # build the output file name
+  output_file = 'RiverSource_' + domains[id] + '.json'
+  # Write the final JSON structure to a JSON file
+  with open(output_file, 'w') as json_file:
+      json.dump(output_data, json_file, indent=4)
+    
+  print(f"JSON file created: {output_file}")
+
